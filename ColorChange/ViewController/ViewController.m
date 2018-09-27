f1c8c2e077f1ee48319b73b117ac607b89dc23bf
wangjin <>
2018-09-27 16:00:59 +0800

    gif add

--- ColorChange/ViewController/ViewController.m
+++ ColorChange/ViewController/ViewController.m
@@ -44,23 +44,7 @@
 }
 
 
-- (void)dataInit {
-    
-//    NSArray *colorArr = @[[UIColor redColor],[UIColor purpleColor],[UIColor orangeColor],[UIColor yellowColor],[UIColor orangeColor],[UIColor purpleColor],[UIColor redColor]];
-//
-//    [self.colorDataArr addObjectsFromArray:colorArr];
-    
-    
-    NSArray *firstArray = @[UIColorFromRGBAlpha(0xB765DB, 1.0),UIColorFromRGBAlpha(0x655EE0, 1.0)];
-    NSArray *secondArray = @[UIColorFromRGBAlpha(0xE871AC, 1.0),UIColorFromRGBAlpha(0xEE9666, 1.0)];
-    NSArray *thirdArray = @[UIColorFromRGBAlpha(0x5FC1DE, 1.0),UIColorFromRGBAlpha(0x726AFF, 1.0)];
-    NSArray *fourthArray = @[UIColorFromRGBAlpha(0x5BCED8, 1.0),UIColorFromRGBAlpha(0x009CD4, 1.0)];
-    NSArray *fifthArray = @[UIColorFromRGBAlpha(0xEEB557, 1.0),UIColorFromRGBAlpha(0xEE9C57, 1.0)];
-
-    NSArray *colorArr = @[firstArray,secondArray,fifthArray,fourthArray,thirdArray];
-    
-    [self.colorDataArr addObjectsFromArray:colorArr];
-}
+ 
 
 
 - (void)createUI{
